d1e5545594f8054b089efcf77155c9226fe830c0
Jakob Boye <jakobboye@gmail.com>
2024-12-10 04:53:18 -0500

    Done

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -9,14 +9,25 @@ def emotion_detector(text_to_analyse):
     myobj = {"raw_document": {"text": text_to_analyse}}
     response = requests.post(URL, json = myobj, headers=HEADERS)
 
-    formatted_response = json.loads(response.text)
-    to_return = formatted_response['emotionPredictions'][0]['emotion']
-    dominant_emotion = ''
-    dominant_score = 0
-    for emotion, score in to_return.items():
-        if score > dominant_score:
-            dominant_emotion = emotion
-            dominant_score = score
-    to_return.update({'dominant_emotion': dominant_emotion})
+    if response.status_code == 400:
+        to_return = {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+            }
+
+    elif response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        to_return = formatted_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = ''
+        dominant_score = 0
+        for emotion, score in to_return.items():
+            if score > dominant_score:
+                dominant_emotion = emotion
+                dominant_score = score
+        to_return.update({'dominant_emotion': dominant_emotion})
 
     return to_return
--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+"""
+This is a Flask app that lets you do an emotion analysis of an input text string.
+"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,26 +9,32 @@ from EmotionDetection.emotion_detection import emotion_detector
 app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
-def sent_analyzer():
+def emotion_analyzer():
+    """
+    Returns an emotion analysis of input string.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     result = emotion_detector(text_to_analyze)
-    return_string = "For the given statement, the system response is "+\
-        f"'anger': {result['anger']}, "+\
-        f"'disgust': {result['disgust']}, "+\
-        f"'fear': {result['fear']}, "+\
-        f"'joy': {result['joy']} and "+\
-        f"'sadness': {result['sadness']}. "+\
-        f"The dominant emotion is {result['dominant_emotion']}."
+
+    if result['dominant_emotion'] is None:
+        return_string = "Invalid text! Please try again!"
+    else:
+        return_string = "For the given statement, the system response is "+\
+            f"'anger': {result['anger']}, "+\
+            f"'disgust': {result['disgust']}, "+\
+            f"'fear': {result['fear']}, "+\
+            f"'joy': {result['joy']} and "+\
+            f"'sadness': {result['sadness']}. "+\
+            f"The dominant emotion is {result['dominant_emotion']}."
+
     return return_string
 
 @app.route("/")
 def render_index_page():
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
-    '''
+    """
+    This function initiates the rendering of the main application page over the Flask channel
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''
     app.run(port=5000)
