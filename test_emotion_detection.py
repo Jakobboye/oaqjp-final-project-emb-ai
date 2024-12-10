fcc246e4a312d0ad9e429da3cbb140d82d00514e
Jakob Boye <jakobboye@gmail.com>
2024-12-10 04:10:48 -0500

    Test done

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,10 +1,12 @@
 import unittest
-from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
+from EmotionDetection.emotion_detection import emotion_detector
 
 class TestSentimentAnalyzer(unittest.TestCase):
-    def test_sentiment_analyzer(self):
-        self.assertEqual(sentiment_analyzer('I am glad this happened')['label'], 'SENT_POSITIVE')
-        self.assertEqual(sentiment_analyzer('I hate working with Pyhton')['label'], 'SENT_NEGATIVE')
-        self.assertEqual(sentiment_analyzer('I am neutral on Python')['label'], 'SENT_NEUTRAL')
+    def test_emotion_detector(self):
+        self.assertEqual(emotion_detector('I am glad this happened')['dominant_emotion'], 'joy')
+        self.assertEqual(emotion_detector('I am really mad about this')['dominant_emotion'], 'anger')
+        self.assertEqual(emotion_detector('I feel disgusted just hearing about this')['dominant_emotion'], 'disgust')
+        self.assertEqual(emotion_detector('I am so sad about this')['dominant_emotion'], 'sadness')
+        self.assertEqual(emotion_detector('I am really afraid that this will happen')['dominant_emotion'], 'fear')
 
 unittest.main()
